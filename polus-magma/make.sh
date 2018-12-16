c266278793f872b0e6f7d8532c2cdd898f199fb3
Mikhail Maslo <maslomihail@gmail.com>
2018-12-16 14:47:24 +0300

    extneded include

--- polus-magma/make.sh
+++ polus-magma/make.sh
@@ -1,3 +1,3 @@
 #! bin/sh
 
-gcc -I /polusfs/soft/magma-2.4.0_open_blas/include main.c -o main
+gcc -I /polusfs/soft/magma-2.4.0_open_blas/include -L /polusfs/soft/magma-2.4.0_open_blas/lib main.c -o main
