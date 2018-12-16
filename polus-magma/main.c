17dd52bbf80f3d36873f88f96fc8131777821239
Mikhail Maslo <maslomihail@gmail.com>
2018-12-16 14:25:27 +0300

    added magma include

--- polus-magma/main.c
+++ polus-magma/main.c
@@ -1,15 +1,14 @@
-//
-//  main.c
-//  polus-magma
-//
-//  Created by Mikhail Maslo on 16/12/2018.
-//  Copyright © 2018 Mikhail Maslo. All rights reserved.
-//
-
 #include <stdio.h>
+#include "magma.h"
+#include "magma_lapack.h"
+
+void init() {
+    magma_init();
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    init();
+    printf("Hello world!");
+    
     return 0;
 }
