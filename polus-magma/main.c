850f7256f151afa5023cf6f65c7f0233b1da90cc
Mikhail Maslo <maslomihail@gmail.com>
2018-12-16 23:51:52 +0300

    added matrix population

--- polus-magma/main.c
+++ polus-magma/main.c
@@ -23,6 +23,10 @@ double randNumber() {
     return (double)rand() / randDenominator;
 }
 
+void print(const magmaDoubleComplex* A, magma_int_t size) {
+    magma_zprint(size, size, A, size);
+}
+
 //#ifdef RELEASE
 void init() {
     magma_init();
@@ -38,8 +42,8 @@ void calculate(int size) {
     magma_int_t mSize = zSize * zSize;
     
     printf("Will init matrix of size %d\n", mSize);
-    magmaFloatComplex *matrix;
-    magma_int_t result = magma_malloc_pinned( (void**) &matrix, size * sizeof(magmaFloatComplex));
+    magmaDoubleComplex *matrix;
+    magma_int_t result = magma_malloc_pinned( (void**) &matrix, size * sizeof(magmaDoubleComplex));
     if (result) {
         printf("Error on allocation, result %d", result);
         return;
@@ -50,30 +54,30 @@ void calculate(int size) {
 //
 //    lapackf77_zlarnv(&idist, ISEED, &mSize, matrix);
     
-    magma_zprint(zSize, zSize, matrix, zSize);
     printf("Allocated with elements count %d\n", mSize);
-//    double max = 0;
-//    for (int i = 0; i < size * size; i++) {
-//        int index = i;
-//        double real = randNumber();
-//        double imaginary = randNumber();
-//        magmaDoubleComplex number = make_cuDoubleComplex(real, imaginary);
-//        matrix[index] = number;
-//        printf("real: %lf, imaginary: %lf, index: %d", real, imaginary, index);
-//        double distance = MAGMA_Z_ABS(number);
-//        if (distance > max) {
-//            max = distance;
-//        }
-//    }
-//
-//    printf("%d", max);
-//
-//    for(int i = 0; i < size; ++i) {
-//        int index = i + size * i;
-//        matrix[index] = MAGMA_Z_ADD(matrix[index], MAGMA_Z_MAKE(max, max));
-//    }
-//
-//    print(matrix, size);
+    double max = 0;
+    for (int i = 0; i < size * size; i++) {
+        int index = i;
+        double real = randNumber();
+        double imaginary = randNumber();
+        magmaDoubleComplex number = make_cuDoubleComplex(real, imaginary);
+        matrix[index] = number;
+        printf("real: %lf, imaginary: %lf, index: %d", real, imaginary, index);
+        double distance = MAGMA_Z_ABS(number);
+        if (distance > max) {
+            max = distance;
+        }
+    }
+    
+    printf("max is %d", max);
+    print(matrix, zSize);
+
+    for(int i = 0; i < size; ++i) {
+        int index = i + size * i;
+        matrix[index] = MAGMA_Z_ADD(matrix[index], MAGMA_Z_MAKE(max, max));
+    }
+
+    print(matrix, size);
     
 //    magmaDoubleComplex *lWork, *tau;
 //    magma_int_t nb = magma_get_zgeqrf_nb(size, size);
