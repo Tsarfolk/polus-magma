70b5db6bccff6d775594e16e6f6e711b280c08c4
Mikhail Maslo <maslomihail@gmail.com>
2018-12-16 14:53:06 +0300

    added cuda path

--- polus-magma/make.sh
+++ polus-magma/make.sh
@@ -1,3 +1,3 @@
 #! bin/sh
 
-gcc -I /polusfs/soft/magma-2.4.0_open_blas/include -L /polusfs/soft/magma-2.4.0_open_blas/lib main.c -o main
+gcc -I /polusfs/soft/magma-2.4.0_open_blas/include -I /usr/local/cuda-9.1/targets/ppc64le-linux/include main.c -o main
