daf7b18951b54be82f1baa18aa71cba91ff38283
Mikhail Maslo <maslomihail@gmail.com>
2018-12-16 14:07:52 +0300

    initial

---
+++ polus-magma/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  polus-magma
+//
+//  Created by Mikhail Maslo on 16/12/2018.
+//  Copyright © 2018 Mikhail Maslo. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
